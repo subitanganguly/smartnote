6ef0eb307a63caffe777e24135094049d9fae259
subitanganguly <101342918+subitanganguly@users.noreply.github.com>
2024-12-21 12:13:05 +0530

    Add files via upload

---
+++ Brain Buster/js/all.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCxnQKUOaUm-BQuHsZY1yUPymf8bfis5Q8",
+  databaseURL: "https://brain-buster-85fdf-default-rtdb.asia-southeast1.firebasedatabase.app",
+};
+
+
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCm5Du9eIWktzK_BF9NTzBNv4SrkgyyDaA",
+//   databaseURL: "https://toributikweb-default-rtdb.firebaseio.com",
+// };
